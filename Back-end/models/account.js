1ab59d1e3dbc47f2328ec8a1668dc62a95771136
owenbalnaves-james <89376767+owenbalnaves-james@users.noreply.github.com>
2023-09-04 20:52:40 +1000

    Revert "Merge branch 'main' of https://github.com/owenbalnaves-james/AdvancedSoftwareDevelopment"
    This reverts commit a85166ba10fcba7c15114403c0eceeaa2ae9ec62, reversing
    changes made to 46d734e01bc35b60b6c816c958c7c63ff0d8581d.

--- Back-end/models/account.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const accountSchema = mongoose.Schema({
-    email: {type:String},
-	password:{type: String}
-})
-
-module.exports = mongoose.model("Account", accountSchema);
