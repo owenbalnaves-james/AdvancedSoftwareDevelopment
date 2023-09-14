790cfa185fad767a0dcc3c655bb72b1e889e011e
hoanglamtran <lamtiensinh2301@gmail.com>
2023-09-14 23:32:01 +1000

    Hoang Lam's commit. Reset Password worked.

--- Back-end/Routes/accounts.js
+++ Back-end/Routes/accounts.js
@@ -9,4 +9,12 @@ router.post("", (req,res,next)=>{
         }
     });
 })
+router.post("/resetpassword",(req,res,next) => {
+    const {email, password} = req.body;
+    Account.updateOne({ email }, { $set: { password: password } }).then(() => {
+        res.status(200).json({
+            message:"Update Successfully"
+        })
+    }).catch(err => console.log(err))
+})
 module.exports = router;
