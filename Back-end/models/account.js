de279d6644e1d17c1831235cb41467f5735230c7
hoanglamtran <lamtiensinh2301@gmail.com>
2023-10-09 22:48:03 +1100

    Lam's Commit. Feature 2 finished.

--- Back-end/models/account.js
+++ Back-end/models/account.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const accountSchema = mongoose.Schema({
     email: {type:String},
-	password:{type: String}
+	password:{type: String},
+    phone:{type: String},
+    name:{type:String},
+    cardNumber:{type:String},
+    address:{type:String}
 })
 
 module.exports = mongoose.model("Account", accountSchema);
