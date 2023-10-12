3144ad156db8714226ff3edff4a8ed8981bbe59e
owenbalnaves-james <89376767+owenbalnaves-james@users.noreply.github.com>
2023-10-12 20:45:06 +1100

    Edited readme & finished test case

--- phpunit.php
+++ phpunit.php
@@ -8,22 +8,23 @@ function testCheckMenuNotEmpty()
     $query = "select * from menuitems";
     $result = mysqli_query($con,$query);
     
-    if($result)
-    {
-        if($result && mysqli_num_rows($result)> 0)
-        {
-          while($row = $result->fetch_assoc()) {
-            $row["name"]->assertNotEmpty($row["name"]);
-            $row["category"]->assertNotEmpty($row["category"]);
-            $row["price"]->assertNotEmpty($row["price"]);
-            $row["pickupOnly"]->assertNotEmpty($row["pickupOnly"]);
-            $row["imageLink"]->assertNotEmpty($row["imageLink"]);
-          }
-        }
-        else {
-
-        }
+    try {
+      assert($result);
+      assert(($result && mysqli_num_rows($result)> 0));
+               
+      while($row = $result->fetch_assoc()) {
+        assert($row["name"] != "");
+        assert($row["category"] != "");
+        assert($row["price"] != "");
+        assert($row["pickupOnly"] != "");
+        assert($row["imageLink"] != "");
+      }    
     }
+    catch (Exception $e) {
+      echo $e;
+    }
+    
+    
 }
 
 testCheckMenuNotEmpty();
